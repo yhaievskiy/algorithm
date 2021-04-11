82a31fad1a4126d80393086cfbeedb38886ba9ba
yurii <yhaievskiy@gmail.com>
2021-04-11 17:09:59 +0300

    adding QuickSort method

--- Sorts/Sorts/src/Sorts.java
+++ Sorts/Sorts/src/Sorts.java
@@ -1,4 +1,5 @@
 import sorts.SelectionSort;
+import sorts.QuickSort;
 
 public class Sorts {
     static void printArray(int[] arr){
@@ -6,9 +7,16 @@ public class Sorts {
             System.out.print(arr[i] + " ");
         }
     }
+    static int findLargest(int[] arr, int n) {
+        if(n == 1)
+            return arr[0];
+       return Math.max(arr[n - 1], findLargest(arr, n - 1));
+    } 
     public static void main(String[] args) throws Exception {
-        int[] test = {1, 0, -10, 56, 3};
+        int[] test = {1, 2, 10, 5, 4, 11};
         SelectionSort.selectionSort(test);
-        printArray(test);
+        int[] test2 = {20, -10, 12, 5, 4, 11};
+        QuickSort.quickSort(test2, 0, test2.length - 1);
+        printArray(test2);
     }
 }
